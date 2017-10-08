e2cc617d0ba7522ea123041ce65d282b3a562451
Dave Barner <dbarner@gmail.com>
2017-10-08 17:57:55 -0400

    initial poc

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={ handleClick.bind(this) }>Load Users</button>
+        <div className="Main">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
+
+var results = document.getElementsByClassName('Main');
+
+
+function handleClick(e) {
+  fetch('https://barner-marketplace-api.herokuapp.com/users.json')
+  .then(function(response) {return response.json();})
+  .then(function(json) {
+    json.forEach(function(name) {
+      results[0].innerHTML += name.first_name;
+      results[0].innerHTML += '<br/>';
+    });
+  })
+};
